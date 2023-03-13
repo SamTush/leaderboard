a86953bcfa07a560f731ce2410abe4c6614d7c72
SamTush <samo4421@icloud.com>
2023-03-13 18:53:58 +0300

    Add the setup files for the project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import add from './module/add.js';
+import './styles/main.scss';
+import logo from './assets/logo-orange.png';
+
+const logoImg = document.getElementById('logo');
+logoImg.setAttribute('src', logo);
---
+++ src/module/add.js
@@ -0,0 +1,5 @@
+function print() {
+  console.log('Say hello there 123456789');
+}
+
+export default print;
