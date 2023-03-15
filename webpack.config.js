c0215fb17a29427aec07394f5ba910c196e1bf61
SamTush <samo4421@icloud.com>
2023-03-15 14:34:57 +0300

    Remove / from template path in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: '/src/index.html',
+      template: 'src/index.html',
     }),
   ],
 };
